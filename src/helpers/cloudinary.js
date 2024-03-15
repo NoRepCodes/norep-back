d41333e5030e97dba045d60590813b716de48f59
NoRep <151470071+NoRepCodes@users.noreply.github.com>
2024-03-15 06:12:53 -0400

    Update cloudinary.js

--- src/helpers/cloudinary.js
+++ src/helpers/cloudinary.js
@@ -5,8 +5,7 @@ dotenv.config()
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET,
-    secure:true,
+    api_secret: process.env.API_SECRET
 });
 // cloudinary.config({
 //     cloud_name: 'dtdgl3ajp',
