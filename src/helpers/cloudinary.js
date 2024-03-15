9bae910191605e1f0792ffd35643e0ab18378c4d
NoRep <151470071+NoRepCodes@users.noreply.github.com>
2024-03-15 06:15:40 -0400

    Update cloudinary.js

--- src/helpers/cloudinary.js
+++ src/helpers/cloudinary.js
@@ -2,16 +2,16 @@ import {v2 as cloudinary} from 'cloudinary'
 import dotenv from 'dotenv'
 dotenv.config()
 
-cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET
-});
 // cloudinary.config({
-//     cloud_name: 'dtdgl3ajp',
-//     api_key: '331735467981966',
-//     api_secret: '4iq8RwNvVUkxRGJzVe7YAqiZvjA',
-//     secure:true,
+//     cloud_name: process.env.CLOUD_NAME,
+//     api_key: process.env.API_KEY,
+//     api_secret: process.env.API_SECRET
 // });
+cloudinary.config({
+    cloud_name: 'dtdgl3ajp',
+    api_key: '331735467981966',
+    api_secret: '4iq8RwNvVUkxRGJzVe7YAqiZvjA',
+    secure:true,
+});
 
 export default cloudinary
