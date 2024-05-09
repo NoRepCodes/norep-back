235265e0d5cbe10b6d51f0153585ef5ad9843e91
NoRep <151470071+NoRepCodes@users.noreply.github.com>
2024-05-09 12:50:22 -0400

    Update teamSchema.js
    update to decimal

--- src/models/teamSchema.js
+++ src/models/teamSchema.js
@@ -1,23 +1,38 @@
 import { Schema, model, SchemaTypes } from 'mongoose'
 const objId = SchemaTypes.ObjectId
 
+const wodRes = new Schema({
+    amount_type: String,
+    time: Number,
+    tiebrake: Number,
+    penalty: Number,
+    amount: {
+        type: SchemaTypes.Decimal128,
+        defautl: 0,
+        get: getDec
+    },
+    id: false,
+}, { toJSON: { getters: true } })
+
 const TeamSchema = new Schema({
     name: String,
     category_id: String,
     event_id: String,
-    box:String,
-    wods:[{
-        time:Number,
-        tiebrake:Number,
-        amount:Number,
-        amount_type:String,
-        penalty:Number
-    }]
+    box: String,
+    wods: [wodRes]
 },
     {
+        toJSON: { getters: true },
         timestamps: true,
     }
 );
+
+function getDec(value) {
+    if (typeof value !== 'undefined') {
+        return parseFloat(value.toString());
+    }
+    return value;
+};
 
 export default model('Team', TeamSchema)
 
