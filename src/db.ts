e037723b923d5657abc629165eb3f4de722fa320
Radul19 <radulito19@gmail.com>
2024-08-23 16:24:59 -0400

    2.1 to prod

--- src/db.ts
+++ src/db.ts
@@ -4,7 +4,7 @@ dotenv.config()
 const connectDB = async () => {
   try {
     set("strictQuery", false);
-    await connect("mongodb+srv://norepcode:Crossfit2023@norep.rkqhfdi.mongodb.net/tests")
+    await connect("mongodb+srv://norepcode:Crossfit2023@norep.rkqhfdi.mongodb.net/norep")
     // await connect("mongodb+srv://norepcode:Crossfit2023@norep.rkqhfdi.mongodb.net/norep" ?? '')
       .then(db => console.log('Database is connected'))
       .catch(err => console.log(err))
