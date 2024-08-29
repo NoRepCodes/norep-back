412b040675217a03758913fe2a24f9928181fe31
Radul19 <radulito19@gmail.com>
2024-08-29 01:39:52 -0400

    uri on test again?

--- src/controllers/guest.ts
+++ src/controllers/guest.ts
@@ -17,9 +17,9 @@ export const test: RequestHandler = async (req, res) => {
     if (debug) console.log('#test')
 
     // const result = await Event.find({ _id: "6656396c8f027cee3e114e68", 'categories.teams': { $exists: true, $type: 'array', $ne: [] } })
-    res.send('version 2.1.3')
+    // res.send('version 2.1.3')
     // res.send('NOREP ONLINE')
-    // res.send(process.env.MONGODB_URI)
+    res.send(process.env.MONGODB_URI)
 }
 
 export const getEvents: RequestHandler = async (req, res) => {
