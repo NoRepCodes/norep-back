1503f5103e1e9f40f361f1631f5d22836eb45da9
Raúl Brito <59905423+Radul19@users.noreply.github.com>
2025-01-23 18:41:53 -0400

    Update user.ts

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -209,7 +209,7 @@ export const registerTicket: RequestHandler = async (req, res) => {
   if (debug) console.log("#registerTicket");
   try {
     const { users, category_id, inputs, image, phone } = req.body;
-    if(users.length === 1) inputs.name = ''
+    if(users.length < 2) inputs.name = '@individual'
     
     const result: (EventType & Document) | null = await Event.findOne({
       "categories._id": category_id,
